f9ccf716e42a07a75ec6e6a08ea0570ea630563b
nail-php <malikov.kzn@gmail.com>
2022-02-01 18:27:59 +0300

    lesson 1: добавляем единый класс настроек, который запускается на старте (SetUp bootstrap)

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,7 +10,7 @@ return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log'], // чтоб логи запускались при старте приложения
     'modules' => [],
     'components' => [
         'request' => [
